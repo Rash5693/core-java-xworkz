3db07d2c706185cb456c518dc9b7f6db85e332f4
Rash5693 <rachanaadiga35@gmail.com>
2023-07-13 14:48:53 +0530

    adding files related to crud operations

---
+++ IcecreamTester.java
@@ -0,0 +1,17 @@
+class IcecreamTester
+{
+  public static void main(String args[])
+  {
+   Icecream.addFlavours("Vanilla");
+   Icecream.addFlavours("Chocolate");
+   Icecream.addFlavours("Butter Scotch");
+   Icecream.addFlavours("Black Current");
+   Icecream.addFlavours("Strawberry");
+   Icecream.addFlavours("Mango");
+   Icecream.getFlavours();
+   Icecream.searchFlavourByName("Mango");
+   
+   }
+   }
+   
+   
---
+++ KarnatakaTester.java
@@ -0,0 +1,16 @@
+class KarnatakaTester
+{
+  public static void main(String args[])
+  {
+    Karnataka.addCity("Bangalore");
+	Karnataka.addCity("Udupi");
+	Karnataka.addCity("Mangalore");
+	Karnataka.addCity("Hassan");
+	Karnataka.addCity("Raichur");
+	Karnataka.addCity("Mysore");
+	Karnataka.getCity();
+	Karnataka.deleteCityNames("Raichur");
+	Karnataka.getAllCityNamesPostDeletion();
+	}
+	}
+	
